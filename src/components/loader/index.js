822a0ee861deac74a5692208395601241fb556ad
Bolton <bolton.otieno@gmail.com>
2019-06-20 09:37:40 +0300

    Updates

--- src/components/loader/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Loader } from 'semantic-ui-react'
-
-const PreLoader = () => <Loader active size='large' inline='centered'/>
-
-export default PreLoader;
--- src/root/rootSagas.js
+++
@@ -1,8 +0,0 @@
-import { all } from 'redux-saga/effects';
-import { watchCategoriesLoad, watchRandomCategoryLoad, watchSearchLoad } from "../application/jokes/saga";
-
-function* rootSaga() {
-  yield all([watchCategoriesLoad(), watchRandomCategoryLoad(), watchSearchLoad()])
-}
-
-export default rootSaga;
